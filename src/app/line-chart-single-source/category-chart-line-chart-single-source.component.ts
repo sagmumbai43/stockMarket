57b5d555161740049fb10fb20ca53f36c17471ce
sagmumbai43 <sagmumbai43@gmail.com>
2022-01-04 00:28:38 +0530

    Initial commit

---
+++ src/app/line-chart-single-source/category-chart-line-chart-single-source.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-chart-line-chart-single-source',
+  templateUrl: './category-chart-line-chart-single-source.component.html',
+  styleUrls: ['./category-chart-line-chart-single-source.component.css']
+})
+export class CategoryChartLineChartSingleSourceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
